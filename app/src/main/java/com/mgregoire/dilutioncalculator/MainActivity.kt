440b41f93373c4a06932871243652ac2728ef0f5
greg-mathieu <mathieugregoire@sfr.fr>
2020-07-29 08:57:55 +0200

    First draft in MainActivity.kt

--- app/src/main/java/com/mgregoire/dilutioncalculator/MainActivity.kt
+++ app/src/main/java/com/mgregoire/dilutioncalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mgregoire.dilutioncalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCompute.setOnClickListener(View.OnClickListener {
+            val requiredDose = editTextRequiredDose.text.toString().toDouble()
+            val concentration = editTextConcentration.text.toString().toDouble()
+            val totalVolume = editTextTotalVolume.text.toString().toDouble()
+
+            val result = (totalVolume * requiredDose) / concentration
+            textViewResult.text = result.toString()
+        })
     }
 }
