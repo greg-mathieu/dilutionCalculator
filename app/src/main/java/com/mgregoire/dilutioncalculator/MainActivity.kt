eb39e09334af39f7441dfa72226284bc061e2a8d
greg-mathieu <mathieugregoire@sfr.fr>
2020-07-31 16:40:22 +0200

    correction xml + add Clear button

--- app/src/main/java/com/mgregoire/dilutioncalculator/MainActivity.kt
+++ app/src/main/java/com/mgregoire/dilutioncalculator/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.mgregoire.dilutioncalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +16,30 @@ class MainActivity : AppCompatActivity() {
         buttonCompute.setOnClickListener(View.OnClickListener {
             val requiredDose = editTextRequiredDose.text.toString().toDouble()
             val concentration = editTextConcentration.text.toString().toDouble()
-            val totalVolume = editTextTotalVolume.text.toString().toDouble()
+            val totalVolume = editTextTotalVolume.toString().toDouble()
 
             val result = (totalVolume * requiredDose) / concentration
-            textViewResult.text = result.toString()
+            editTextResult.setText(result.toString())
+
+            hideKeyboard()
         })
+
+        buttonClear.setOnClickListener(View.OnClickListener {
+            clear(editTextRequiredDose)
+            clear(editTextConcentration)
+            clear(editTextTotalVolume)
+            clear(editTextResult)
+        })
+    }
+
+    private fun clear (editText: EditText) {
+        editText.text.clear()
+        editText.clearFocus()
+    }
+
+    // find on "http://programminget.blogspot.com/2017/08/how-to-close-android-soft-keyboard.html"
+    private fun hideKeyboard() {
+        val inputManager:InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED)
     }
 }
